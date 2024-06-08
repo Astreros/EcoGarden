295f136887f719b37b2ccc03f9d0f3a4b8829a03
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-06-08 18:47:43 +0200

    Ajout des fonctions de mise à jour et de suppression d'un utilisateur dans UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,21 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
-    public function __construct(private readonly UserRepository $userRepository,
-                                private readonly SerializerInterface $serializer,
+    public function __construct(private readonly SerializerInterface $serializer,
                                 private readonly ValidatorInterface $validator,
                                 private readonly EntityManagerInterface $entityManager,
                                 private readonly UserPasswordHasherInterface $userPasswordHasher)
@@ -44,15 +44,37 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
     }
 
+    /**
+     * @throws JsonException
+     */
     #[Route('/api/user/{id}', name: 'updateUser', methods: ['PUT'])]
-    public function updateUser(): JsonResponse
+    public function updateUser(Request $request, User $currentUser): JsonResponse
     {
+        $updatedUser = $this->serializer->deserialize($request->getContent(), User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
 
+        $errors = $this->validator->validate($updatedUser);
+        if (count($errors) > 0) {
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        if ($data['password'] !== null) {
+            $updatedUser->setPassword($this->userPasswordHasher->hashPassword($updatedUser, $updatedUser->getPassword()));
+        }
+
+        $this->entityManager->persist($updatedUser);
+        $this->entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/api/user/{id}', name: 'deleteUser', methods: ['DELETE'])]
-    public function deleteUser(): JsonResponse
+    public function deleteUser(User $user): JsonResponse
     {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
 
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
